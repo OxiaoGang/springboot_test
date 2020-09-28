e1a47a400bc577efa6c33c74a9c416f3f0ffd67b
jack <jack@123.com>
2020-09-28 16:44:43 +0800

    aaaaaa

--- src/main/java/com/qa/springboot/controller/Acontroller.java
+++ src/main/java/com/qa/springboot/controller/Acontroller.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Acontroller {
 	@RequestMapping("/a")
 	public Object aaa(){
-		return "success";
+		return "aaaaa";
 	}
 }
