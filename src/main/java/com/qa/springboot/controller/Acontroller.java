b57f674025c4804ed7eae04c8ceb7bd6ad87ac1c
saga <saga@kiva.com>
2020-09-28 16:11:10 +0800

    222222222

---
+++ src/main/java/com/qa/springboot/controller/Acontroller.java
@@ -0,0 +1,13 @@
+package com.qa.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Acontroller {
+	@RequestMapping("/a")
+	public Object aaa(){
+		return "success";
+	}
+}
